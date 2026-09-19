c19f4e8d949acbb7f72f799eda0ec64c7d381205
Chris A <cv565942@gmail.com>
2026-09-18 21:08:48 -0500

    obliterate chiese translations

--- MultiLang/MultiLang.h
+++ MultiLang/MultiLang.h
@@ -2,13 +2,7 @@
 #define MULTILANG
 
 namespace MultiLang {
-#ifdef CHINESE_SIMP // Simplified Chinese
-	#include "SimplifiedChinese.cpp"
-#elif defined CHINESE_TRAD // Traditional Chinese
-	#include "TraditionalChinese.cpp"
-#else // English
 	#include "English.cpp"
-#endif
 };
 
 #endif
--- config/config.h
+++ config/config.h
@@ -4,6 +4,5 @@
 #define BASE_VER	1136
 #define	PRO_F	0x010000
 #define VERSION		(BASE_VER|PRO_F)
-//#define CHINESE_SIMP
 
 #endif
